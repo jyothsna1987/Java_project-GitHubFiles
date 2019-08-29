4cfb3b5ab06138adf68c9b0b07511900a0338a6b
Ubuntu <jyothsna1987@gmail.com>
2019-08-29 05:37:11 +0000

    This is the second commit to the repository

--- application.java
+++ application.java
@@ -2,8 +2,9 @@ class HelloJava {
 
 public static void main(String args[]){
 
-System.out.Println(“ I am your Java Program. Thank you! ”);
+System.out.Println(" I am your Java Program. Thank you! ");
 
+System.out.Println(" Have a good day. Bye! ");
 }
 
 }
